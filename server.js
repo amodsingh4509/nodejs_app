5bbc4309bedc20d2bb45e3fb02a04986998e14f4
amod <amodkumar968@gmail.com>
2023-09-16 13:11:18 +0530

    firs commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/hello', (req, res) => {
+  res.send('Hello all, Welcome to my express Project!')
+})
+// app.use(express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
